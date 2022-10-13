5853ce32b4e82f6423eb808e397b4ca3f1e66c29
PetoKristofIskola <peto.kristof_2020c@bolyai-szakkozep.hu>
2022-10-13 09:42:14 +0200

    txt

--- Project/server.py
+++ Project/server.py
@@ -220,4 +220,4 @@ def handle_context():
     return dict(session=session, db=db, datetime=datetime, reversed=reversed)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=6969, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
